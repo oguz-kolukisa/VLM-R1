e6518b1f424b8717f992bfc7b43dc0afea38460c
okolukisa <oguzkolukisa1@gmail.com>
2025-12-31 10:44:09 +0300

    Update eval_grefcoco_rec.sh

--- eval_grefcoco_rec.sh
+++ eval_grefcoco_rec.sh
@@ -22,7 +22,7 @@ TEST_DATASETS=${TEST_DATASETS:-""}
 OUTPUT_DIR=${OUTPUT_DIR:-"${REPO_HOME}/logs"}
 NUM_GPUS=${NUM_GPUS:-1}
 NUM_SAMPLES=${NUM_SAMPLES:-20}
-BSZ=${BSZ:-8}
+BSZ=${BSZ:-4}
 # ----------------------------------------------------------------------------
 
 mkdir -p "${DATA_DIR}"
